ec99531f965b65616cabf60c6a48b5b34c4a7260
AranBeitia <arantza.beitia@gmail.com>
2021-12-17 18:39:55 +0100

    songs finished

--- src/controllers/song-controller.js
+++ src/controllers/song-controller.js
@@ -2,6 +2,44 @@ const db = require("../models");
 
 async function createSong(req, res, next) {
   try {
+    await db.Song.create(req.body);
+    res.status(200).send({ message: "Song created correctly" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getAllSongs(req, res, next) {
+  try {
+    const data = await db.Song.find({}).exec();
+    res.status(200).send({ message: "Todo ok", data });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getOneSong(req, res, next) {
+  try {
+    const data = await db.Song.findById(req.params.songId);
+    res.status(200).send(data);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateSong(req, res, next) {
+  try {
+    await db.Song.findByIdAndUpdate(req.params.songId, req.body);
+    res.status(200).send(req.body);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteSong(req, res, next) {
+  try {
+    await db.Song.findByIdAndDelete(req.params.songId);
+    res.status(200).send({ message: "Song deleted ok", data: req.body });
   } catch (error) {
     next(error);
   }
@@ -9,4 +47,8 @@ async function createSong(req, res, next) {
 
 module.exports = {
   createSong,
+  getAllSongs,
+  getOneSong,
+  updateSong,
+  deleteSong,
 };
--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -3,5 +3,8 @@ const songController = require("../controllers/song-controller.js");
 const SongRouter = Router();
 
 SongRouter.post("/songs", songController.createSong);
-
+SongRouter.get("/songs", songController.getAllSongs);
+SongRouter.get("/songs/:songId", songController.getOneSong);
+SongRouter.patch("/songs/:songId", songController.updateSong);
+SongRouter.delete("/songs/:songId", songController.deleteSong);
 module.exports = SongRouter;
